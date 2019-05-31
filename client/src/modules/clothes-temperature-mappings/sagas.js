abb94a8ce4e340eecf16f133ad1cdce60d8a48a6
Nayema Nasir <nayema.nasir89@gmail.com>
2019-05-30 22:33:57 -0400

    Import fork and all for sagas

--- client/src/modules/clothes-temperature-mappings/sagas.js
+++ client/src/modules/clothes-temperature-mappings/sagas.js
@@ -1,22 +1,22 @@
-import { put, call, take } from 'redux-saga/effects'
+import { put, call, take, all, fork } from 'redux-saga/effects';
 
-import * as actionTypes from './action-types'
-import * as actionCreators from './action-creators'
-import * as repository from './repository'
+import * as actionTypes from './action-types';
+import * as actionCreators from './action-creators';
+import * as repository from './repository';
 
 function * loadAll () {
-  const clothesTemperatureMappings = yield call(repository.loadAll)
-  yield put(actionCreators.loadAllSucceeded(clothesTemperatureMappings))
+  const clothesTemperatureMappings = yield call(repository.loadAll);
+  yield put(actionCreators.loadAllSucceeded(clothesTemperatureMappings));
 }
 
 function * addClothesTemperatureMapping () {
-  let action
+  let action;
   while (action = yield take(actionTypes.ADD_MAPPING_REQUESTED)) {
     const clothesTemperatureMapping = yield call(
       repository.addClothesTemperatureMapping,
       action.payload
-    )
-    yield put(actionCreators.addMappingSucceeded(clothesTemperatureMapping))
+    );
+    yield put(actionCreators.addMappingSucceeded(clothesTemperatureMapping));
   }
 }
 
@@ -24,7 +24,7 @@ function * sagas () {
   yield all([
     fork(loadAll),
     fork(addClothesTemperatureMapping)
-  ])
+  ]);
 }
 
-export default sagas
+export default sagas;
--- client/src/modules/current-conditions/sagas.js
+++ client/src/modules/current-conditions/sagas.js
@@ -1,17 +1,17 @@
-import { put, call } from 'redux-saga/effects'
+import { put, call, all, fork } from 'redux-saga/effects';
 
-import * as actionCreators from './action-creators'
-import * as repository from './repository'
+import * as actionCreators from './action-creators';
+import * as repository from './repository';
 
 export function * loadCurrentConditions () {
-  const currentConditions = yield call(repository.loadCurrentConditions)
-  yield put(actionCreators.loadCurrentConditionsSucceeded(currentConditions))
+  const currentConditions = yield call(repository.loadCurrentConditions);
+  yield put(actionCreators.loadCurrentConditionsSucceeded(currentConditions));
 }
 
 function * sagas () {
   yield all([
     fork(loadCurrentConditions)
-  ])
+  ]);
 }
 
-export default sagas
+export default sagas;
